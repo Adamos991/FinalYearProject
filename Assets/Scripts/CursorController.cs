6cf0389ba6d96026c3be041d18561bf8bf585f80
Adamos991 <19265808@studentmail.ul.ie>
2023-04-03 20:54:41 +0100

    Yurt

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,40 +1,121 @@
-// using UnityEngine;
-// using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
-// public class CursorController : MonoBehaviour
-// {
-//     public CursorConstraint cursorConstraint;
-//     private RectTransform rectTransform;
-//     private Canvas canvas;
+public class CursorController : MonoBehaviour
+{
+    public List<RectTransform> gridLines;
+    private RectTransform rectTransform;
+    private Vector2 previousMovementDirection;
 
-//     private void Start()
-//     {
-//         rectTransform = GetComponent<RectTransform>();
-//         canvas = GetComponentInParent<Canvas>();
-//         Cursor.visible = true;
-//     }
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Cursor.visible = true;
+        previousMovementDirection = Vector2.zero;
+    }
 
-//     private void Update()
-//     {
-//         if (!this.enabled)
-//         {
-//             Cursor.visible = true;
-//             return;
-//         }
+    private void Update()
+    {
+        if (!this.enabled)
+        {
+            Cursor.visible = true;
+            return;
+        }
 
-//         Cursor.visible = false;
+        Cursor.visible = false;
 
-//         // Get the mouse position in canvas space
-//         Vector2 mousePosition = Input.mousePosition;
-//         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, mousePosition, canvas.worldCamera, out mousePosition);
+        Vector2 newPosition = rectTransform.anchoredPosition;
+        Vector2 cursorCenter = newPosition;
+        bool moved = false;
+        Vector2 movementDirection = Vector2.zero;
 
-//         // Constrain the cursor position and set it
-//         Vector2 newPosition = cursorConstraint.GetConstrainedPosition(rectTransform.anchoredPosition, mousePosition);
-//         rectTransform.anchoredPosition = newPosition;
-//     }
-// }
-// using UnityEngine;
-// using UnityEngine.UI;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            movementDirection = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            movementDirection = Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            movementDirection = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            movementDirection = Vector2.down;
+        }
+
+        if (movementDirection != Vector2.zero)
+        {
+            previousMovementDirection = movementDirection;
+        }
+
+        foreach (RectTransform gridLine in gridLines)
+        {
+            Rect gridLineRect = GetWorldRect(gridLine);
+
+            if (gridLineRect.Contains(cursorCenter))
+            {
+                if (movementDirection == Vector2.left || movementDirection == Vector2.right)
+                {
+                    if(gridLineRect.width > gridLineRect.height) {
+                        newPosition += movementDirection * 1f;
+                        if(gridLineRect.Contains(newPosition)) {
+                            moved = true;
+                        }
+                    } else {
+                        for(int i = 6; i < 12; i++) {
+                            Debug.Log(newPosition.y + ", " + gridLines[i].anchoredPosition.y);
+                            if(Mathf.Abs(newPosition.y - gridLines[i].anchoredPosition.y) <= 10) {
+                                Vector2 targetPosition = new Vector2(newPosition.x, gridLines[i].anchoredPosition.y);
+                                Vector2 direction = targetPosition - newPosition;
+                                direction = direction.normalized;
+                                newPosition += direction * Time.deltaTime;
+                                i--;
+                            }
+                        }
+                        continue;
+                    }
+                }
+                else if (movementDirection == Vector2.up || movementDirection == Vector2.down)
+                {
+                    if(gridLineRect.height > gridLineRect.width) {
+                        newPosition += movementDirection * 1f;
+                        if(gridLineRect.Contains(newPosition)) {
+                            moved = true;
+                        }
+                    } else {
+                        for(int i = 0; i < 6; i++) {
+                            if(Mathf.Abs(newPosition.x - gridLines[i].anchoredPosition.x) <= 10) {
+                                Vector2 targetPosition = new Vector2(gridLines[i].anchoredPosition.x, newPosition.y);
+                                Vector2 direction = targetPosition - newPosition;
+                                direction = direction.normalized;
+                                newPosition += direction * Time.deltaTime;
+                                i--;
+                            }
+                        }
+                        continue;
+                    }
+                }
+                if(moved) {
+                    rectTransform.anchoredPosition = newPosition;
+                    break;
+                }
+            }
+        }
+    }
+
+    private Rect GetWorldRect(RectTransform rectTransform)
+    {
+        Vector3[] corners = new Vector3[4];
+        rectTransform.GetWorldCorners(corners);
+        Vector2 min = new Vector2(corners[0].x, corners[0].y);
+        Vector2 max = new Vector2(corners[2].x, corners[2].y);
+        return new Rect(min, max - min);
+    }
+}
 
 // public class CursorController : MonoBehaviour
 // {
@@ -59,90 +140,3 @@
 //         rectTransform.anchoredPosition = mousePosition;
 //     }
 // }
-
-using UnityEngine;
-using UnityEngine.UI;
-using System.Collections.Generic;
-public class CursorController : MonoBehaviour
-{
-    public List<RectTransform> gridLines;
-    private RectTransform rectTransform;
-
-    private void Start()
-    {
-        rectTransform = GetComponent<RectTransform>();
-        Cursor.visible = true;
-    }
-
-    private void Update()
-    {
-        if (!this.enabled)
-        {
-            Cursor.visible = true;
-            return;
-        }
-
-        Cursor.visible = false;
-
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector2 newPosition = rectTransform.anchoredPosition;
-        Vector2 cursorCenter = newPosition + new Vector2(rectTransform.rect.width / 2f, -rectTransform.rect.height / 2f);
-
-        bool moved = false;
-
-        foreach (RectTransform gridLine in gridLines)
-        {
-            Rect gridLineRect = GetWorldRect(gridLine);
-
-            if (gridLineRect.Contains(cursorCenter))
-            {
-                Debug.Log(horizontal + " ||| " + gridLineRect.width);
-                if (horizontal != 0 && gridLineRect.width > gridLineRect.height)
-                {
-                    Debug.Log("bloop5");
-                    newPosition.x += horizontal * gridLineRect.width;
-                    moved = true;
-                }
-                else if (vertical != 0 && gridLineRect.height > gridLineRect.width)
-                {
-                    Debug.Log("bloop6");
-                    newPosition.y += vertical * gridLineRect.height;
-                    moved = true;
-                }
-
-                if (horizontal != 0 && vertical != 0 && !moved)
-                {
-                    Debug.Log("bloop3");
-                    float horizontalDistance = Mathf.Min(Mathf.Abs(cursorCenter.x - gridLineRect.xMin), Mathf.Abs(cursorCenter.x - gridLineRect.xMax));
-                    float verticalDistance = Mathf.Min(Mathf.Abs(cursorCenter.y - gridLineRect.yMin), Mathf.Abs(cursorCenter.y - gridLineRect.yMax));
-
-                    if (horizontalDistance < verticalDistance && gridLineRect.width > gridLineRect.height)
-                    {
-                        Debug.Log("bloop1");
-                        newPosition.x += horizontal * gridLineRect.width;
-                    }
-                    else if (verticalDistance < horizontalDistance && gridLineRect.height > gridLineRect.width)
-                    {
-                        Debug.Log("bloop2");
-                        newPosition.y += vertical * gridLineRect.height;
-                    }
-                }
-
-                break;
-            }
-        }
-        //Debug.Log(newPosition);
-        rectTransform.anchoredPosition = newPosition;
-    }
-
-    private Rect GetWorldRect(RectTransform rectTransform)
-    {
-        Vector3[] corners = new Vector3[4];
-        rectTransform.GetWorldCorners(corners);
-        Vector2 min = new Vector2(corners[0].x, corners[0].y);
-        Vector2 max = new Vector2(corners[2].x, corners[2].y);
-        return new Rect(min, max - min);
-    }
-}
